b269305239294d942f3b67de78d124647755bddc
Chuck Konkol <ckonkol@gmail.com>
2020-05-08 11:16:11 -0500

    Create links.js

---
+++ links.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    var url = get(“url”);
+    window.open (' + url + ,’_self',false);
+
+};
+
+function get(name){
+    var url = window.location.search;
+    var num = url.search(name);
+    var namel = name.length;
+    var frontlength = namel+num+1; //length of everything before the value
+    var front = url.substring(0, frontlength);
+    url = url.replace(front, "");
+    num = url.search("&");
+    if(num>=0) return url.substr(0,num);
+    if(num<0)  return url;
+}
+
+
+get("list");
