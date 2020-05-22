7c11f1d47f114c8cb5a45b0fe8b1224ef0eec951
Chuck Konkol <ckonkol@gmail.com>
2020-05-22 12:55:40 -0500

    Update links.js

--- links.js
+++ links.js
@@ -1,5 +1,6 @@
 window.onload = function() {
-    var url = get("url");
+    var url = window.location.search;
+    //var url = get("url");
     alert("This link will be opened up in new tab. \r\nMake sure popup blocker is turned off for this website (usually in URL address bar).\t\nTry again once unblocked - Professor Konkol, RVC");
     window.open(url, '_blank');
 };
